70a74d0cee445b0c983673269b32ec7730af8e82
unknown <95763161+SGJki@users.noreply.github.com>
2023-10-11 14:30:56 +0800

    initial commit

---
+++ web1/src/test/java/cp/cas/back/Web1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cp.cas.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
